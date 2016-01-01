41a36c0c51ba5caf9002613ae95e6a8c6085fffe
flying code <563375002@qq.com>
2016-01-01 18:23:57 +0800

    v1.0

---
+++ src/com/base/Business.java
@@ -0,0 +1,65 @@
+package com.base;
+
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.Connection;
+
+
+
+public class Business {
+	
+	
+	private static Connection conn = null;
+	private static List<Connection> conns = new ArrayList<Connection>();
+	
+	static{
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/login", "root", "root");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static Connection getConn(){
+		return conn;
+	}
+	
+	public static void main(String[] args) {
+		Connection conn = Business.getConn();
+		System.out.println(conn);
+		 
+	}
+
+
+
+
+	public static String getPassword(String username){
+		Statement st;
+		String sql = "select * from user where username='"+username+"'";     // 查询数据的sql语句  
+		String password = "";
+		try {
+			st = (Statement) conn.createStatement();
+			ResultSet rs = st.executeQuery(sql);    //执行sql查询语句，返回查询数据的结果集  
+	        while (rs.next()) { // 判断是否还有下一个数据  
+	            // 根据字段名获取相应的值  
+	            password = rs.getString("password");  
+	        }  
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}    //创建用于执行静态sql语句的Statement对象，st属局部变量  
+		return password;
+	}
+
+}
+
---
+++ src/servlet/LoginServlet.java
@@ -0,0 +1,87 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.base.Business;
+import com.po.User;
+
+public class LoginServlet extends HttpServlet {
+
+	/**
+	 * Constructor of the object.
+	 */
+	public LoginServlet() {
+		super();
+	}
+
+	/**
+	 * Destruction of the servlet. <br>
+	 */
+	public void destroy() {
+		super.destroy(); // Just puts "destroy" string in log
+		// Put your code here
+	}
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		doPost(request,response);
+	}
+
+	/**
+	 * The doPost method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to post.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		User user = new User();
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		user.setUsername(username);
+		user.setPassword(password);
+		
+	    String pass = "";
+	    pass = Business.getPassword(username);
+	    if(pass.equals(password)&&!pass.equals("")){
+	    	request.getRequestDispatcher("/LoginSuccess.jsp").forward(request, response); 
+//	    	response.sendRedirect(request.getContextPath()+"/LoginSuccess.jsp");
+	    }else{
+	    	request.getRequestDispatcher("/LoginFailed.jsp").forward(request, response); 
+//	    	response.sendRedirect(request.getContextPath()+"/LoginFailed.jsp");
+	    }
+	}
+
+	/**
+	 * Initialization of the servlet. <br>
+	 *
+	 * @throws ServletException if an error occurs
+	 */
+	public void init() throws ServletException {
+		// Put your code here
+	}
+
+}
